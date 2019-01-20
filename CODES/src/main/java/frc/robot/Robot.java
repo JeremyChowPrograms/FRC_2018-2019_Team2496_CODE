1e262d8a4cbb7deebbf44aa4075336d555a3558f
Jeremy Chow <jeremychow02@gmail.com>
2019-01-20 15:27:14 -0800

    Annotation Added

--- CODES/src/main/java/frc/robot/Robot.java
+++ CODES/src/main/java/frc/robot/Robot.java
@@ -34,22 +34,26 @@ public class Robot extends TimedRobot {
   private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
   private Compressor compressor;
+  //Light sensors for auto-correction
   private DigitalLib left, mid, right;
 
   private DoubleSolenoid lift1, lift2, intake;
 
+  //False if not attached, will error if set true but cam not attached
   private boolean useCamera = false;
   private double liftHeight = 0.0d;
 
   public Robot() {
-    chassis = new ChassisControl(3, 4, 1, 2);// Changed temporarily, normal is 0,1,2,3
   }
 
   @Override
   public void robotInit() {
+    //Ports according to design table
+    chassis = new ChassisControl(3, 4, 1, 2);
     gyro.reset();
     gyro.calibrate();
     gyro.reset();
+    //The params for constructors might change due to design and such, change if required
     liftEncoder = new Encoder(0, 1);
     leftE = new Encoder(2, 3);
     rightE = new Encoder(4, 5);
@@ -81,6 +85,7 @@ public class Robot extends TimedRobot {
     encoderForward(72, 0.4);
   }
 
+  //These are the macros that can be used in either auton or teleop
   private void leftTurn(double degrees, double speed) {
     gyro.reset();
     double doPid = 0;
@@ -137,7 +142,7 @@ public class Robot extends TimedRobot {
     Timer.delay(0.1);
     chassis.tankDrive(0, 0);
   }
-
+  
   // Init Code
   @Override
   public void teleopInit() {
@@ -146,6 +151,7 @@ public class Robot extends TimedRobot {
       Thread t = new Thread() {
         @Override
         public void run() {
+          //Doing PID for lift
           PIDLib pid = new PIDLib(0.3, 0, 0.03, 0.0);
           liftEncoder.reset();
           while (isTeleop) {
@@ -172,11 +178,13 @@ public class Robot extends TimedRobot {
     }
     // Op region
     claw.set(leftJoyStick.getZ());
+    //Lift and intake pneumatics
     lift1.set(leftJoyStick.getRawButton(2) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
     lift2.set(leftJoyStick.getRawButton(3) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
     intake.set(leftJoyStick.getRawButton(4) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
-    // Macro
+    // Macro, Driving
     {
+      //If hold button 3, will do auto-correct lining up codes, else joystick controll
       if (rightJoyStick.getRawButton(3) && mid.get()) {
         if (left.get())
           chassis.tankDrive(0.3, 0.2);
