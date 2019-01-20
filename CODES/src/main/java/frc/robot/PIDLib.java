e6146798abfff475732eaa8b9736d53d2be0ed70
Jeremy Chow <jeremychow02@gmail.com>
2019-01-20 13:34:08 -0800

    Robot Code Prototype For Testing Purpose

--- CODES/src/main/java/frc/robot/PIDLib.java
+++ CODES/src/main/java/frc/robot/PIDLib.java
@@ -2,18 +2,19 @@ package frc.robot;
 
 public class PIDLib {
     private double kp, ki, kd;
-    private double target, error;
+    private double target, error = 0.0d;
     private double errorprev;
-    private double integral, integral_lim, derivative;
+    private double integral = 0.0d, integral_lim, derivative;
 
-    public PIDLib(double kp, double ki, double kd, double integral_lim, double target) {
+    public PIDLib(double kp, double ki, double kd, double integral_lim) {
         this.kp = kp;
         this.ki = ki;
         this.kd = kd;
-        this.target = target;
+        this.integral_lim = integral_lim;
     }
 
-    public double PID(double input) {
+    public double PID(double input, double target) {
+        this.target = target;
         this.errorprev = this.error;
         this.error = this.target - input;
         if (this.error < integral_lim && this.error > -integral_lim) {
--- CODES/src/main/java/frc/robot/Robot.java
+++ CODES/src/main/java/frc/robot/Robot.java
@@ -9,7 +9,12 @@ package frc.robot;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,19 +24,43 @@ public class Robot extends TimedRobot {
   private Joystick leftJoyStick, rightJoyStick;
   private ChassisControl chassis;
   private UsbCamera camera;
+  private Encoder liftEncoder;
+  private SpeedController liftMotor;
+  private SpeedController claw;
   private boolean isTeleop = false;
+  private Compressor compressor;
+  private DigitalLib left, mid, right;
+
+  private DoubleSolenoid lift1, lift2, intake;
+
+  private boolean useCamera = false;
+  private double liftHeight = 0.0d;
 
   public Robot() {
-    chassis = new ChassisControl(0, 0, 0, 0);// Changed temporarily, normal is 0,1,2,3
+    chassis = new ChassisControl(3, 4, 1, 2);// Changed temporarily, normal is 0,1,2,3
   }
 
   @Override
   public void robotInit() {
+    liftEncoder = new Encoder(0, 1);
+    liftEncoder.setDistancePerPulse(Math.PI / 200);
     leftJoyStick = new Joystick(kJoystickPort);
     rightJoyStick = new Joystick(kJoystickPort2);
-    camera = CameraServer.getInstance().startAutomaticCapture();
-    camera.setResolution(800, 600);
-    camera.setFPS(60);
+    claw = new Talon(5);
+    liftMotor = new Talon(6);
+    compressor = new Compressor();
+    compressor.setClosedLoopControl(true);
+    lift1 = new DoubleSolenoid(0, 1);
+    lift2 = new DoubleSolenoid(2, 3);
+    intake = new DoubleSolenoid(4, 5);
+    left = new DigitalLib(0, 1);
+    mid = new DigitalLib(2, 3);
+    right = new DigitalLib(4, 5);
+    if (useCamera) {
+      camera = CameraServer.getInstance().startAutomaticCapture();
+      camera.setResolution(800, 600);
+      camera.setFPS(60);
+    }
   }
 
   // Auton code here, used Init because only run once
@@ -43,13 +72,13 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     isTeleop = this.isOperatorControl();
-    // New Thread
     {
       Thread t = new Thread() {
         @Override
         public void run() {
+          PIDLib pid = new PIDLib(0.3, 0, 0.03, 0.0);
           while (isTeleop) {
-
+            liftMotor.set(pid.PID(liftEncoder.getDistance(), liftHeight));
           }
         }
       };
@@ -60,8 +89,28 @@ public class Robot extends TimedRobot {
   // Teleop code here
   @Override
   public void teleopPeriodic() {
-    chassis.tankDrive(leftJoyStick.getZ(), rightJoyStick.getY());
-    SmartDashboard.setDefaultNumber("Value", leftJoyStick.getZ());
-    isTeleop = this.isOperatorControl();
+    // Info, nop region
+    {
+      SmartDashboard.setDefaultNumber("Value", leftJoyStick.getZ());
+      isTeleop = this.isOperatorControl();
+    }
+    // Op region
+    claw.set(leftJoyStick.getZ());
+    lift1.set(leftJoyStick.getRawButton(2) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+    lift2.set(leftJoyStick.getRawButton(3) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+    intake.set(leftJoyStick.getRawButton(4) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+    // Macro
+    {
+      if (rightJoyStick.getRawButton(3) && mid.get()) {
+        if (left.get())
+          chassis.tankDrive(0.3, 0.2);
+        else if (right.get())
+          chassis.tankDrive(0.2, 0.3);
+        else
+          chassis.tankDrive(0.25, 0.25);
+      } else {
+        chassis.tankDrive(leftJoyStick.getY(), rightJoyStick.getY());
+      }
+    }
   }
 }
